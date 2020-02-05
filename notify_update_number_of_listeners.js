49bfc55bc917f4a7f65d661ad75a9d20140e5ac5
JeYA9JoHojZtZYG <60560683+JeYA9JoHojZtZYG@users.noreply.github.com>
2020-02-05 12:59:34 +0900

    cleanup

--- notify_update_number_of_listeners.js
+++ notify_update_number_of_listeners.js
@@ -1,13 +1,13 @@
-javascript:(()=> {
-    let getFraction = () => parseInt(document.querySelector(".tw-fraction").textContent.split("/")[0]);
+(()=> {
+    let getFraction = () => parseInt(document.querySelector('.tw-fraction').textContent.split('/')[0]);
     let oldFraction = getFraction();
     let newFraction;
     let intervalId = setInterval(() => {
         newFraction = getFraction();
         if (newFraction > oldFraction) {
-        new Notification(newFraction + "名様いらっしゃい");
+        new Notification(newFraction + '名様いらっしゃい');
         }
         oldFraction = newFraction;
     }, 2000);
-    console.log("Interval ID = " + intervalId);
+    console.log('Interval ID = ' + intervalId);
 })();
